0aa0cbf14d1b8129d4707ad47057e8dac751f2df
Geraldine Lagarrigue <lagarriguegeraldine@gmail.com>
2026-08-20 18:49:42 -0300

    parte 1

---
+++ gestor-publicaciones/usuario.js
@@ -0,0 +1,13 @@
+class Usuario {
+    constructor(nombre, email){
+        this.nombre = nombre;
+        this.email = email;
+        this.fechaRegistro = new Date();
+    }
+
+    mostrarPerfil(){
+        return `${this.nombre} - ${this.email}`;
+    }
+}
+
+export default Usuario;
